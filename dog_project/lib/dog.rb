43d89c80cec159e89b632cac6683c39d0cfcb4ce
Shelo <YOUR@EMAIL.com>
2019-06-11 10:29:33 +0800

    submit

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,39 @@
 class Dog
+    def initialize(name,breed,age,bark,favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        return @name
+    end
+    def name=(name)
+        @name = name
+    end
+    def breed
+        return @breed
+    end
+    def age
+        return @age
+    end
+    def age=(age)
+        @age = age
+    end
+    def bark
+        if @age <= 3
+            @bark.downcase
+        else
+            @bark.upcase
+        end
+    end
+    def favorite_foods
+        return @favorite_foods
+    end
+    def favorite_food?(string)
+        @favorite_foods.to_s.downcase.include?(string.downcase)
+    end
 
 end
